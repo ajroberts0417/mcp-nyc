6d46c56b30ccd242cb4a5d2ff1425ecb3538cc9e
rjeli <eli@rje.li>
2024-12-11 19:56:29 -0500

    council members

--- src/tools.ts
+++ src/tools.ts
@@ -1,33 +1,33 @@
-import { queryLegistar } from "./legistar.js";
+import { queryCouncilMembers, queryLegistar } from "./legistar.js";
 
 type ToolRequestHandlerOutput = {
-    content: {
-      type: 'text' | 'json';
-      text: string;
-      mimeType: 'text/plain' | 'application/json';
-    }[]
-  }
+  content: {
+    type: 'text' | 'json';
+    text: string;
+    mimeType: 'text/plain' | 'application/json';
+  }[]
+}
 
-  type ToolRequest = {
-    params: {
-        name: string
-        arguments?: Record<string, unknown>
-    }
+type ToolRequest = {
+  params: {
+    name: string
+    arguments?: Record<string, unknown>
   }
+}
   
-  type ToolSchema = {
-      name: string;
-      description: string;
-      inputSchema: {
-        type: 'object';
-        properties: Record<string, { type: 'string' }>; // other types are available, but i don't know what they are now.
-      };
-      requestHandler: (request: ToolRequest) => Promise<ToolRequestHandlerOutput>;
-    }
+type ToolSchema = {
+  name: string;
+  description: string;
+  inputSchema: {
+    type: 'object';
+    properties: Record<string, { type: 'string' }>; // other types are available, but i don't know what they are now.
+  };
+  requestHandler: (request: ToolRequest) => Promise<ToolRequestHandlerOutput>;
+}
     
 
-export function addTool(tool: ToolSchema){
-    TOOLS.push(tool)
+export function addTool(tool: ToolSchema) {
+  TOOLS.push(tool)
 }
 
 export const TOOLS: ToolSchema[] = [
@@ -35,25 +35,46 @@ export const TOOLS: ToolSchema[] = [
 ]
 
 addTool({
-    name: 'run_legistar_api',
-    description: 'Run a legistar api query',
-    inputSchema: {
-      type: 'object',
-      properties: {
-        legistar_intent: { type: 'string' },
-      },
-    },
-    async requestHandler(params) {
-      console.error("legister query???", params);
-      const data = await queryLegistar()
-      return {
-        content: [{
-          type: 'text',
-          text: data,
-          mimeType: 'text/plain'
-        }],
-      };
-    }
-  })
+  name: 'get_matters',
+  description: 'get current matters',
+  inputSchema: {
+    type: 'object',
+    properties: {},
+  },
+  async requestHandler(params) {
+    console.error("legister query???", params);
+    const data = await queryLegistar()
+    return {
+      content: [{
+        type: 'text',
+        text: data,
+        mimeType: 'text/plain'
+      }],
+    };
+  }
+})
 
   
+addTool({
+  name: 'get_council_members',
+  description: 'Get council members',
+  inputSchema: {
+    type: 'object',
+    properties: {
+      top: { type: 'string' },
+      skip: { type: 'string' },
+      activeOnly: { type: 'string' },
+    },
+  },
+  async requestHandler(req) {
+    console.error("legister query???", req);
+    const data = await queryCouncilMembers(req.params.arguments);
+    return {
+      content: [{
+        type: 'text',
+        text: data,
+        mimeType: 'text/plain'
+      }],
+    };
+  }
+});
